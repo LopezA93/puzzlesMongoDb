d9ad235f20d65fa0b325243d6ed3607f2d519416
LopezA93 <voodoo.lux@gmail.com>
2022-12-27 19:23:44 -0300

    vercel config

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -6,6 +6,7 @@ const PASSWORD = process.env.PASSWORD;
 const ejs = require("ejs");
 const path = require("path");
 
+
 const transporter = nodemailer.createTransport({
   service: "gmail",
   port: 587,
@@ -16,40 +17,41 @@ const transporter = nodemailer.createTransport({
 });
 
 const mailOrden = async (user, order) => {
-  const requiredPath = path.join(__dirname, "../views/pages/OrderCreated.ejs");
+  // const requiredPath = path.join(__dirname, "../views/pages/OrderCreated.ejs");
 
-  const data = await ejs.renderFile(requiredPath, {
-    nombre: user,
-    order: order,
+  // const data = await ejs.renderFile(requiredPath, {
+  //   nombre: user,
+  //   order: order
+  // });
+  const prods = order.productos.map((i) => {
+    return (i = {
+      nombre: i.nombre,
+      precio: i.precio,
+      cantidad: i.cantidad,
+    });
   });
-  //   const prods = order.productos.map((i) => {
-  //     return (i = {
-  //       nombre: i.nombre,
-  //       precio: i.precio,
-  //       cantidad: i.cantidad,
-  //     });
-  //   });
-  //   const items = JSON.stringify(prods);
+  const items = JSON.stringify(prods);
   try {
     // console.log("prods,", prods);
     const info = await transporter.sendMail({
       to: user,
       from: EMAIL,
       subject: "Nueva orden generada de Puzzles",
-      html: data,
-      //   `<h3> Le informamos que se ha confirmado la orden de compra realizada con los siguientes datos:</h3>
-      //     <p> Orden número: ${order.numero} </p>
-      //     <p> Productos: ${items} </p>
-      //     <p> Total: ${order.total} </p>
-      //     <p> Desde ya muchas gracias por su compra. Cualquier duda o consulta no dude en contactarnos.</p>
-
-      //      `,
+      html:
+      //  data
+      `<h3> Le informamos que se ha confirmado la orden de compra realizada con los siguientes datos:</h3> 
+        <p> Orden número: ${order.numero} </p>
+        <p> Productos: ${items} </p>
+        <p> Total: ${order.total} </p>
+        <p> Desde ya muchas gracias por su compra. Cualquier duda o consulta no dude en contactarnos.</p>
+        
+         `,
     });
     console.log(`Se genero el envio de mail de la orden: ${order.numero}`);
   } catch (error) {
     console.log(`Error en el envio de la orden: ${error}`);
   } finally {
-    console.log("Order creada");
+    console.log('Order creada')
   }
 };
 module.exports = mailOrden;
