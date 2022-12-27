1b744ddff03a3e2b0d3cf0d946bc0b1390a60ed6
LopezA93 <voodoo.lux@gmail.com>
2022-12-27 18:53:31 -0300

    html mail

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -17,42 +17,36 @@ const transporter = nodemailer.createTransport({
 });
 
 const mailOrden = async (user, order) => {
-  const requiredPath = path.join(__dirname, "../views/pages/OrderCreated.ejs");
+  // const requiredPath = path.join(__dirname, "../views/pages/OrderCreated.ejs");
 
-  const data = await ejs.renderFile(requiredPath, {
-    nombre: user,
-    order: order
+  // const data = await ejs.renderFile(requiredPath, {
+  //   nombre: user,
+  //   order: order
+  // });
+  const prods = order.productos.map((i) => {
+    return (i = {
+      nombre: i.nombre,
+      precio: i.precio,
+      cantidad: i.cantidad,
+    });
   });
-//   const prods = order.productos.map((i) => {
-//     return (i = {
-//       nombre: i.nombre,
-//       precio: i.precio,
-//       cantidad: i.cantidad,
-//     });
-//   });
-//   const items = JSON.stringify(prods);
+  const items = JSON.stringify(prods);
   try {
     // console.log("prods,", prods);
     const info = await transporter.sendMail({
       to: user,
       from: EMAIL,
       subject: "Nueva orden generada de Puzzles",
-      html: data
-    //   `<h3> Le informamos que se ha confirmado la orden de compra realizada con los siguientes datos:</h3> 
-    //     <p> Orden número: ${order.numero} </p>
-    //     <p> Productos: ${items} </p>
-    //     <p> Total: ${order.total} </p>
-    //     <p> Desde ya muchas gracias por su compra. Cualquier duda o consulta no dude en contactarnos.</p>
+      html: 
+      // data
+      `<h3> Le informamos que se ha confirmado la orden de compra realizada con los siguientes datos:</h3> 
+        <p> Orden número: ${order.numero} </p>
+        <p> Productos: ${items} </p>
+        <p> Total: ${order.total} </p>
+        <p> Desde ya muchas gracias por su compra. Cualquier duda o consulta no dude en contactarnos.</p>
         
-    //      `,
-    }, (err, info) => {
-      if (err) {
-        console.error(err.message);
-      } else {
-        res.json(info.response);
-        console.log(`Se genero el envio de mail de la orden: ${order.numero}`);
-      }}
-      )
+         `,
+    })
     
   } catch (error) {
     console.log(`Error en el envio de la orden: ${error}`);
