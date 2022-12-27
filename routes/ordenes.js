d40624ac2290c1699e84ce7a7e156648ab771990
LopezA93 <voodoo.lux@gmail.com>
2022-12-27 18:44:29 -0300

    middleware order

--- routes/ordenes.js
+++ routes/ordenes.js
@@ -3,9 +3,14 @@ const {Router} = express;
 const {generarOrden, getOrderByUser} = require('../controllers/ordenes')
 const ordenRoute = Router()
 const cors = require('cors')
+ordenRoute.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://puzzlesmongodb.netlify.app")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 //CRUD
 
 ordenRoute.post('/', generarOrden)
-ordenRoute.get('/:email', cors({origin:'*'}), getOrderByUser)
+ordenRoute.get('/:email', getOrderByUser)
 
 module.exports = ordenRoute
